fa6dc3a87ae764a71641966603b006928abe4eeb
Ryad <ryadus2001@gmail.com>
2019-10-22 09:45:31 +0200

    twig exo

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+require '../vendor/autoload.php';
+$products = ['product1','product2','product3','product4','products5'];
+
+
+$loader = new Twig\Loader\FilesystemLoader('../src/View');
+$twig = new Twig\Environment($loader);
+echo $twig->render('index.html.twig',[
+    'products'=> $products
+]);
